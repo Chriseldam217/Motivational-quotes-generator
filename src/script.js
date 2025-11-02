5990d085038344b9e2c2a88e39911b1ff4b8d3e2
Chriseldam217 <chriselda.melissa@icloud.com>
2025-11-02 22:48:40 +0200

    added axios on html, edited my js and included api

--- src/script.js
+++ src/script.js
@@ -1,13 +1,27 @@
-function generateMotivation(event) {
-  event.preventDefault();
-
+function displayMotivation(response) {
   new Typewriter("#motivation", {
-    strings:
-      "Perseverance is the steadfast drive to continue a difficult task despite obstacles",
+    strings: [response.data.answer],
     autoStart: true,
     delay: 1,
     cursor: "",
   });
+}
+
+function generateMotivation(event) {
+  event.preventDefault();
+
+  let userGuideInput = document.querySelector("#user-guidance");
+  let apiKey = "d48c015a82tfo433bbe90a92b0d3714a";
+  let context =
+    "You are a powerful motivation expert and love to write short motivations.Your mission is to generate a 4 line motivation in basic HTML and separate each line with a <br />. Make sure to follow the user instructions.";
+  let prompt = `User instructions: Generate a motivation about ${userGuideInput.value}`;
+  let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  let motivationElement = document.querySelector("#motivation");
+  motivationElement.classList.remove("hidden");
+  motivationElement.innerHTML = `<div class="generating">⏳ Generating a motivation about ${userGuideInput.value}</div>`;
+
+  axios.get(apiURL).then(displayMotivation);
 }
 
 let motivationFormElement = document.querySelector(
